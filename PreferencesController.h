4378934139edd69120732e7e6d00424a9688bb03
Stephen F. Booth <me@sbooth.org>
2005-10-02 16:10:42 +0000

    Moving stuff around

---
+++ PreferencesController.h
@@ -0,0 +1,50 @@
+/*
+ *  $Id$
+ *
+ *  Copyright (C) 2005 Stephen F. Booth <me@sbooth.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#import <Cocoa/Cocoa.h>
+
+@interface PreferencesController : NSObject
+{
+    IBOutlet NSTextField			*_customNameTextField;
+    IBOutlet NSTextField			*_cddbServerTextField;
+    IBOutlet NSTextField			*_cddbPortTextField;
+
+    IBOutlet NSWindow				*_window;
+
+	IBOutlet NSArrayController		*_cddbMirrorsController;
+
+	NSArray							*_cddbMirrors;
+    NSString						*_customNameExample;
+}
+
++ (PreferencesController *) sharedPreferences;
+
+- (void)showPreferencesWindow;
+
+- (IBAction)customNamingButtonAction:(id)sender;
+
+- (IBAction)setFreeDBMirror:(id)sender;
+
+- (IBAction)restoreDefaults:(id)sender;
+
+- (IBAction)selectOutputDirectory:(id)sender;
+- (void)openPanelDidEnd:(NSOpenPanel *)sheet returnCode:(int)returnCode contextInfo:(void *)contextInfo;
+
+@end
