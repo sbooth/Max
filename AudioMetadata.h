e136db3ac04b50842ab494f67a9a272264074280
Stephen F. Booth <me@sbooth.org>
2005-12-16 03:54:03 +0000

    Renamed and made into class

---
+++ AudioMetadata.h
@@ -0,0 +1,44 @@
+/*
+ *  $Id: Track.h 202 2005-12-04 21:50:52Z me $
+ *
+ *  Copyright (C) 2005 Stephen F. Booth <me@sbooth.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#import <Cocoa/Cocoa.h>
+
+@interface AudioMetadata : NSObject
+{
+	NSNumber		*_trackNumber;
+	NSString		*_trackTitle;
+	NSString		*_trackArtist;
+	NSNumber		*_trackYear;
+	NSString		*_trackGenre;
+	NSString		*_trackComment;
+
+	NSNumber		*_albumTrackCount;
+	NSString		*_albumTitle;
+	NSString		*_albumArtist;
+	NSNumber		*_albumYear;
+	NSString		*_albumGenre;
+	NSString		*_albumComment;
+
+	NSNumber		*_multipleArtists;
+	NSNumber		*_discNumber;
+	NSNumber		*_discsInSet;
+}
+
+@end
