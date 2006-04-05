060682b1db64486555c4d78c014d78a073b5b57d
Stephen F. Booth <me@sbooth.org>
2006-04-05 07:09:55 +0000

    Album art export with custom naming

---
+++ AlbumArtPreferencesController.m
@@ -0,0 +1,100 @@
+/*
+ *  $Id$
+ *
+ *  Copyright (C) 2005, 2006 Stephen F. Booth <me@sbooth.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#import "AlbumArtPreferencesController.h"
+
+enum {
+	kDiscNumberButton		= 0,
+	kDiscsInSetButton		= 1,
+	kDiscArtistButton		= 2,
+	kDiscTitleButton		= 3,
+	kDiscGenreButton		= 4,
+	kDiscYearButton			= 5
+};
+
+@implementation AlbumArtPreferencesController
+
+- (id) init
+{
+	if((self = [super initWithWindowNibName:@"AlbumArtPreferences"])) {
+		return self;		
+	}
+	return nil;
+}
+
+- (void) awakeFromNib
+{
+	// Update the example track text field
+	[self controlTextDidChange:nil];
+}
+
+- (IBAction)customNamingButtonAction:(id)sender
+{
+	NSString *string;
+	
+	switch([(NSButton *)sender tag]) {
+		case kDiscNumberButton:			string = @"{discNumber}";		break;
+		case kDiscsInSetButton:			string = @"{discsInSet}";		break;
+		case kDiscArtistButton:			string = @"{discArtist}";		break;
+		case kDiscTitleButton:			string = @"{discTitle}";		break;
+		case kDiscGenreButton:			string = @"{discGenre}";		break;
+		case kDiscYearButton:			string = @"{discYear}";			break;
+	}
+	
+	NSText *fieldEditor = [_customNameTextField currentEditor];
+	if(nil == fieldEditor) {
+		[_customNameTextField setStringValue:string];
+	}
+	else {
+		if([_customNameTextField textShouldBeginEditing:fieldEditor]) {
+			[fieldEditor replaceCharactersInRange:[fieldEditor selectedRange] withString:string];
+			[_customNameTextField textShouldEndEditing:fieldEditor];
+			[self controlTextDidChange:nil];
+		}
+	}
+}
+
+#pragma mark Delegate methods
+
+- (void) controlTextDidChange:(NSNotification *)aNotification
+{
+	NSString *scheme = [_customNameTextField stringValue];
+	if(nil == scheme) {
+		scheme = [[NSUserDefaults standardUserDefaults] stringForKey:@"albumArtNamingScheme"];
+	}
+	// No love
+	if(nil == scheme) {
+		return;
+	}
+	
+	NSMutableString *sample = [NSMutableString stringWithCapacity:[scheme length]];
+	[sample setString:scheme];		
+	
+	[sample replaceOccurrencesOfString:@"{discNumber}"		withString:@"" options:nil range:NSMakeRange(0, [sample length])];
+	[sample replaceOccurrencesOfString:@"{discsInSet}"		withString:@"" options:nil range:NSMakeRange(0, [sample length])];
+	[sample replaceOccurrencesOfString:@"{discArtist}"		withString:@"Nirvana" options:nil range:NSMakeRange(0, [sample length])];
+	[sample replaceOccurrencesOfString:@"{discTitle}"		withString:@"MTV Unplugged in New York" options:nil range:NSMakeRange(0, [sample length])];
+	[sample replaceOccurrencesOfString:@"{discGenre}"		withString:@"Grunge" options:nil range:NSMakeRange(0, [sample length])];
+	[sample replaceOccurrencesOfString:@"{discYear}"		withString:@"1994" options:nil range:NSMakeRange(0, [sample length])];
+	
+	[self setValue:sample forKey:@"customNameExample"];
+}
+
+@end
