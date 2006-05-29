6a6e358fac85ea865aed500e31c08a8f732aeff6
Stephen F. Booth <me@sbooth.org>
2006-05-29 16:25:20 +0000

    Tweak encoder settings description

--- Encoders/OggVorbisEncoder.m
+++ Encoders/OggVorbisEncoder.m
@@ -405,11 +405,11 @@ static int sVorbisBitrates [14] = { 48, 56, 64, 80, 96, 112, 128, 160, 192, 224,
 {
 	switch(_mode) {
 		case VORBIS_MODE_QUALITY:
-			return [NSString stringWithFormat:@"libVorbis settings: VBR(q=%f)", _quality * 10.f];
+			return [NSString stringWithFormat:@"Ogg Vorbis settings: VBR(q=%f)", _quality * 10.f];
 			break;
 			
 		case VORBIS_MODE_BITRATE:
-			return [NSString stringWithFormat:@"libVorbis settings: %@(%l kbps)", (_cbr ? @"CBR" : @"VBR"), _bitrate / 1000];
+			return [NSString stringWithFormat:@"Ogg Vorbis settings: %@(%l kbps)", (_cbr ? @"CBR" : @"VBR"), _bitrate / 1000];
 			break;
 			
 		default:
