c1975298014fda8f425ecb3ce55467d77d6d8408
Stephen F. Booth <me@sbooth.org>
2006-10-30 06:12:32 +0000

    MAC returns LE, not BE, data

--- Decoders/MonkeysAudioDecoder.mm
+++ Decoders/MonkeysAudioDecoder.mm
@@ -60,7 +60,7 @@
 
 	// Setup input format descriptor
 	_pcmFormat.mFormatID			= kAudioFormatLinearPCM;
-	_pcmFormat.mFormatFlags			= kAudioFormatFlagIsSignedInteger | kAudioFormatFlagIsBigEndian | kAudioFormatFlagIsPacked;
+	_pcmFormat.mFormatFlags			= kAudioFormatFlagIsSignedInteger | kAudioFormatFlagIsPacked;
 	
 	_pcmFormat.mSampleRate			= SELF_DECOMPRESSOR->GetInfo(APE_INFO_SAMPLE_RATE);
 	_pcmFormat.mChannelsPerFrame	= SELF_DECOMPRESSOR->GetInfo(APE_INFO_CHANNELS);
