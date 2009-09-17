837a9fa45ed4bc1dadd2c474ce1c72560f2bc747
Stephen F. Booth <me@sbooth.org>
2009-09-17 22:16:36 +0000

    Upgraded to new mp4v2 library

---
+++ Frameworks/mp4v2/mp4v2-src/include/mp4v2/project.h
@@ -0,0 +1,31 @@
+#ifndef MP4V2_PROJECT_H
+#define MP4V2_PROJECT_H
+
+/*****************************************************************************/
+
+#define MP4V2_PROJECT_name            "MP4v2"
+#define MP4V2_PROJECT_name_lower      "mp4v2"
+#define MP4V2_PROJECT_name_upper      "MP4V2"
+#define MP4V2_PROJECT_name_formal     "MP4v2 trunk-r369"
+#define MP4V2_PROJECT_url_website     "http://code.google.com/p/mp4v2"
+#define MP4V2_PROJECT_url_downloads   "http://code.google.com/p/mp4v2/downloads/list"
+#define MP4V2_PROJECT_url_discussion  "http://groups.google.com/group/mp4v2"
+#define MP4V2_PROJECT_irc             "irc://irc.freenode.net/handbrake"
+#define MP4V2_PROJECT_bugreport       "<eddyg@myreflection.org>"
+#define MP4V2_PROJECT_version         "trunk-r369"
+#define MP4V2_PROJECT_version_hex     0x00020000
+#define MP4V2_PROJECT_version_major   2
+#define MP4V2_PROJECT_version_minor   0
+#define MP4V2_PROJECT_version_point   0
+#define MP4V2_PROJECT_repo_url        "http://mp4v2.googlecode.com/svn/trunk"
+#define MP4V2_PROJECT_repo_branch     "trunk"
+#define MP4V2_PROJECT_repo_root       "http://mp4v2.googlecode.com/svn"
+#define MP4V2_PROJECT_repo_uuid       "6e6572fa-98a6-11dd-ad9f-f77439c74b79"
+#define MP4V2_PROJECT_repo_rev        369
+#define MP4V2_PROJECT_repo_date       "2009-07-13 16:34:46 -0700 (Mon, 13 Jul 2009)"
+#define MP4V2_PROJECT_repo_type       "developer"
+#define MP4V2_PROJECT_build           "Wed Sep 16 23:03:08 PDT 2009"
+
+/*****************************************************************************/
+
+#endif /* MP4V2_PROJECT_H */
---
+++ Frameworks/mp4v2/mp4v2-src/libplatform/config.h
@@ -0,0 +1,59 @@
+/* libplatform/config.h.  Generated from config.h.in by configure.  */
+/* libplatform/config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if LFS should be activated */
+/* #undef NEED_LFS_ACTIVATION */
+
+/* Name of package */
+#define PACKAGE "mp4v2"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "eddyg@myreflection.org"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "MP4v2"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "MP4v2 trunk-r369"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "mp4v2"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "trunk-r369"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "trunk-r369"
