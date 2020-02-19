4edcfe3cb2aa885b4954d28f8ceb20fe67e043c2
Stephen F. Booth <me@sbooth.org>
2020-02-19 16:47:30 -0600

    Initialize seconds to 0 before using

--- Formatters/SecondsFormatter.m
+++ Formatters/SecondsFormatter.m
@@ -1,7 +1,5 @@
 /*
- *  $Id$
- *
- *  Copyright (C) 2005 - 2007 Stephen F. Booth <me@sbooth.org>
+ *  Copyright (C) 2005 - 2020 Stephen F. Booth <me@sbooth.org>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -67,7 +65,7 @@
 	NSScanner		*scanner		= nil;
 	BOOL			result			= NO;
 	int				value			= 0;
-	unsigned		seconds;
+	unsigned		seconds			= 0;
 
 	scanner		= [NSScanner scannerWithString:string];
 	
