5c17e3dfa2e1bd03f95dc93a5f2a7b2d5ffaf001
Stephen F. Booth <me@sbooth.org>
2020-02-22 11:18:59 -0600

    Removed svn keywords

--- Decoders/DecoderMethods.h
+++ Decoders/DecoderMethods.h
@@ -1,6 +1,4 @@
 /*
- *  $Id$
- *
  *  Copyright (C) 2007 Stephen F. Booth <me@sbooth.org>
  *
  *  This program is free software; you can redistribute it and/or modify
--- Utilities/GaplessUtilities.h
+++ Utilities/GaplessUtilities.h
@@ -1,6 +1,4 @@
 /*
- *  $Id$
- *
  *  Copyright (C) 2005 - 2009 Stephen F. Booth <me@sbooth.org>
  *
  *  This program is free software; you can redistribute it and/or modify
